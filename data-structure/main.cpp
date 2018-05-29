db668162c4a600eca929c8bc5cb2869e8147af1c
letangers <2910787416@qq.com>
2018-05-30 02:08:07 +0800

    增加了链表的测试

--- data-structure/main.cpp
+++ data-structure/main.cpp
@@ -1,9 +1,11 @@
-#include "seqlist.cpp"
+//#include "seqlist.cpp"
+#include "linklist.cpp"
 #include <iostream>
 
 using namespace std;
 
 int main(void){
+	/*--------------seqlist-------------
 	seqlist<int> a(3);
 	cout<<"当前长度"<<a.length()<<endl;	
 	cout<<"最大长度"<<a.maxlength()<<endl;
@@ -22,5 +24,24 @@ int main(void){
 	int *tempget2=a.get(2);
 	a.del(2);
 	cout<<*tempget1<<" "<<templocate<<" "<<*tempget2<<" "<<a.length()<<endl;
+	-----------------------------------*/
+	int temp=0;
+	linklist<int> a(&temp);
+	cout<<"表长"<<a.length()<<endl;
+	node<int> *arg=new node<int>;
+	arg->data=1;
+	a.insert(1,arg);
+	node<int> *ret;
+	a.get(0,&ret);
+	cout<<ret->data<<" ";
+	a.get(1,&ret);
+	cout<<ret->data<<endl;
+	temp=5;
+	a.change(0,&temp);
+	a.get(0,&ret);
+	cout<<ret->data<<endl;
+	a.del(0);
+	a.get(0,&ret);
+	cout<<ret->data<<endl;
 	return 0;
 }
